458047077c5069f15d1d5bee9632ab7853605bf9
Saad Personal <saad.zahid66@gmail.com>
2026-09-24 02:54:08 +0300

    Custom-1

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -2,18 +2,10 @@
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 
-#define RED_LED_NODE DT_ALIAS(ext_red_led)
-
-static const struct gpio_dt_spec red_led = GPIO_DT_SPEC_GET(RED_LED_NODE, gpios);
 
 int main()
 {
-    gpio_pin_configure_dt(&red_led, GPIO_OUTPUT_INACTIVE);
-
-    while(1) {
-        gpio_pin_toggle_dt(&red_led);
-        k_msleep(CONFIG_LED_BLINK_TIME_MS);
-    }
+    printf("Hello World!\n");
 
     return 0;
 
